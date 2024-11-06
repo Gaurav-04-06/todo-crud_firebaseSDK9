df103c4262f745993dbb74d0546fcc75853bbc5a
Gaurav-04-06 <gauravkohli204@gmail.com>
2024-11-06 23:12:35 +0530

    running

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyCYQm6KAZhT65pxJPsVHUYAuEP_-9DxIYk",
+  authDomain: "todo-app-5f7d8.firebaseapp.com",
+  projectId: "todo-app-5f7d8",
+  storageBucket: "todo-app-5f7d8.firebasestorage.app",
+  messagingSenderId: "192045572165",
+  appId: "1:192045572165:web:e33cca803cba18060e032c",
+  measurementId: "G-3NHQ83X5JY"
 };
 
 const app = initializeApp(firebaseConfig);
